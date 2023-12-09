f3bb5ed637591540e673079f269e49908832a5fe
icefun <im@icefun.icu>
2023-12-09 18:13:02 +0800

    Nacos注册中心和配置

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -18,7 +18,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         //2.利用RestTemplate发起http请求
-        String url="http://USERSERVER/user/"+ order.getUserId();
+        String url="http://UserServer/user/"+ order.getUserId();
         //发送请求
         User user = restTemplate.getForObject(url, User.class);
         //封装
